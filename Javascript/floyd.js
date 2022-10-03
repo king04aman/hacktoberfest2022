66f0b9bc64342c450d06f94d7f7879742c0f0241
Akkichau <92620285+Akkichau@users.noreply.github.com>
2022-10-03 10:47:15 +0530

    Create floyd.js

---
+++ Javascript/floyd.js
@@ -0,0 +1,15 @@
+//HACKTOBERFEST-ACCEPTED
+//HACKTOBERFEST-ACCEPTED2022
+
+
+let i,j,k=1,n=5,string=""
+for(i=1;i<=n;i++)
+    {
+   for(j=1;j<=i;j++,k++)
+  {
+    string=string+k
+    // console.log( k)
+  }
+ string=string+"\n"
+  }
+    console.log(string)
