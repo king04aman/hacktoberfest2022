6d5b642f9dceaec4211a1718d5ab2765e3fce17a
speedcoder121 <114097593+speedcoder121@users.noreply.github.com>
2022-10-04 09:44:59 +0530

    Create reverse_a_string.cpp

---
+++ CPP/reverse_a_string.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to reverse a string
+void reverseStr(string& str)
+{
+	int n = str.length();
+
+	// Swap character starting from two
+	// corners
+	for (int i = 0; i < n / 2; i++)
+		swap(str[i], str[n - i - 1]);
+}
+
+// Driver program
+int main()
+{
+	string str = "Abhijit ";
+	reverseStr(str);
+	cout << str;
+	return 0;
+}
