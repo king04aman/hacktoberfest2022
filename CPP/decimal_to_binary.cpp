5e2d1ba459c3d2cee0a09b137eeea976c4ca845a
Ankit Singh <singhankits2003@gmail.com>
2022-10-08 01:48:13 +0530

    Added Comments and few code fixes

--- CPP/decimal_to_binary.cpp
+++ CPP/decimal_to_binary.cpp
@@ -1,18 +1,19 @@
 // Program Written by minhaj-313
-#include <iostream>
+#include <bits/stdc++.h> //A header file which includes each and every library of C++
 using namespace std;
 
-int main() 
+int main()
 {
-  int decimal, binary = 0, remainder, product = 1;
+  int decimal, binary = 0, remainder, product = 1,n;
   cin >> decimal;
-  while (decimal != 0) 
+  n=decimal;
+  while (decimal != 0) //While number is not equal to zero
   {
-    remainder = decimal % 2;
-    binary = binary + (remainder * product);
+    remainder = decimal % 2; //Store the remainder, when number is divided by 2
+    binary = binary + (remainder * product); //Forming the number in binary form
     decimal = decimal / 2;
     product *= 10;
   }
-  cout << "The number in the binary form is: " << binary ;
+  cout << "Binary form of "<<n<<" is: "<< binary;
   return 0;
 }
