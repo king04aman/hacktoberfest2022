517c53f339d78016cd6cae830ee08758ff0d4be7
Kunal Bhavare <89301840+kbhavre@users.noreply.github.com>
2022-10-08 22:07:28 +0530

    Create amst_no.py

---
+++ Python/amst_no.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a 3 digit number : "))
+ 
+s = 0
+temp = num
+
+while temp > 0:
+    digit = temp % 10
+    s += digit * digit * digit
+    temp = temp//10
+ 
+if s==num:
+    print("It's an Armstrong number")
+else:
+    print("It's not an Armstrong number")
