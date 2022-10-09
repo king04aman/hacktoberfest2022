068824e02b7b6c5474e686d25779e1a6cd243232
himanshu17-2000 <64321714+himanshu17-2000@users.noreply.github.com>
2022-10-09 14:53:50 +0530

    Updated Firebase

--- Javascript/BankApplication/src/Firebase.js
+++ Javascript/BankApplication/src/Firebase.js
@@ -1,1 +1,11 @@
-// please add your  firebase credentials here to access the database 
+ import firebase from "firebase";
+ // add your firebase credentials here , ThankYou
+ var firebaseConfig = {
+
+  };
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore()
+  const rtdb = firebase.database()
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+  export {db , app ,timestamp ,rtdb}
