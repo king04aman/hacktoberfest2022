4d9c58ae91dc24d9549e6a1e3794c4bc2045e558
photosoftrips <115392419+photosoftrips@users.noreply.github.com>
2022-10-10 11:41:56 +0530

    Create 3SumClosest.cpp

---
+++ 3SumClosest.cpp
@@ -0,0 +1,24 @@
+ int threeSumClosest(vector<int>& nums, int target) {
+            if(nums.size() < 3) return 0;
+    int closest = nums[0]+nums[1]+nums[2];
+    sort(nums.begin(), nums.end());
+    for(int first = 0 ; first < nums.size()-2 ; ++first) {
+        if(first > 0 && nums[first] == nums[first-1]) continue;
+        int second = first+1;
+        int third = nums.size()-1;            
+        while(second < third) {
+            int curSum = nums[first]+nums[second]+nums[third];
+            if(curSum == target) return curSum;
+            if(abs(target-curSum)<abs(target-closest)) {
+                closest = curSum;
+            }
+            if(curSum > target) {
+                --third;
+            } else {
+                ++second;
+            }
+        }
+    }
+    return closest;
+
+    }
