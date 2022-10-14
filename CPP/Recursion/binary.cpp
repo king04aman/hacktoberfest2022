78242136350c7a128e69b5e5565ce08abf2f2df1
Supratick Ghosh <72028007+Supratick2001@users.noreply.github.com>
2022-10-14 20:37:24 +0530

    binary.cpp updated

--- CPP/Recursion/binary.cpp
+++ CPP/Recursion/binary.cpp
@@ -1,35 +1,58 @@
 #include <iostream>
 using namespace std;
 
-int getSum(int arr[], int size)
+int BinarySearch(int arr[], int num, int beg, int end)
 {
-    // base case
-    if (size == 0)
-    {
-        return 0;
-    }
-
-    if (size == 1)
-    {
-        return arr[0];
-    }
-
-    // Progressing case
-    arr = arr + 1;
-    size = size - 1;
-
-    // recursive case
-    int remainPart = getSum(arr, size);
-    int sum = arr[0] + remainPart;
-    return sum;
+ int mid;
+ 
+ if (beg > end){
+  
+  cout << "Number is not found";
+  return 0;
+  
+ } else {
+  
+  mid = (beg + end) / 2;
+  
+  if(arr[mid] == num){
+   
+   cout << "Number is found at " << mid << " index \n";
+   return 0;
+  
+  } else if (num > arr[mid]) {
+   
+   BinarySearch (arr, num, mid+1, end);
+   
+  } else if (num < arr[mid]) {
+   
+   BinarySearch (arr, num, beg , mid-1);
+  }
+ }
+ 
+ 
 }
-
-int main()
-{
-    int arr[] = {10, 10, 10, 10, 10};
-    int size = 5;
-    //  function call
-    int sum = getSum(arr, size);
-    cout << "Sum of Array : " << sum;
-    return 0;
+ 
+int main() {
+ 
+ int arr[100], num, i, n, beg, end;
+ 
+ cout <<"Enter the size of an array (Max 100) \n";
+ cin >> n;
+ 
+ cout <<"Enter the sorted values \n";
+ 
+ for(i=0; i<n; i++) {
+  
+  cin >> arr[i];
+ }
+ 
+ cout <<"Enter a value to be search \n";
+ cin >> num;
+ 
+ beg = 0;
+ end = n-1;
+ 
+ BinarySearch (arr, num, beg, end);
+ 
+ return 0;
 }
