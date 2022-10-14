6400445b64e4f46b6f100f591c8ca5d512c41bd6
Supratick Ghosh <72028007+Supratick2001@users.noreply.github.com>
2022-10-14 20:40:43 +0530

    binarysearch.cpp updated

--- CPP/Recursion/binarySearch.cpp
+++ CPP/Recursion/binarySearch.cpp
@@ -1,45 +1,58 @@
 #include <iostream>
 using namespace std;
 
-bool binarySearch(int arr[], int s, int e, int key)
+int BinarySearch(int arr[], int num, int beg, int end)
 {
-    int mid = s+(e-s)/2;
-    // base case
-    // element not found
-    if (s > e)
-    {
-        return false;
-    }
-    // element found
-    if(arr[mid] ==key){
-        return true;
-    }
-
-    // int mid = s + (e - s) / 2;
-    if (arr[mid] < key)
-    {
-        binarySearch(arr, mid + 1, e, key);
-    }
-    else
-    {
-        binarySearch(arr, s, mid-1, key);
-    }
+ int mid;
+ 
+ if (beg > end){
+  
+  cout << "Number is not found";
+  return 0;
+  
+ } else {
+  
+  mid = (beg + end) / 2;
+  
+  if(arr[mid] == num){
+   
+   cout << "Number is found at " << mid << " index \n";
+   return 0;
+  
+  } else if (num > arr[mid]) {
+   
+   BinarySearch (arr, num, mid+1, end);
+   
+  } else if (num < arr[mid]) {
+   
+   BinarySearch (arr, num, beg , mid-1);
+  }
+ }
+ 
+ 
 }
-
-int main()
-{
-    int arr[] = {1, 2, 3, 4, 5, 6};
-    int size = 6;
-    int key = 1;
-    bool ans = binarySearch(arr, 0, 5, key);
-    cout<<"Present or Not : "<<binarySearch(arr, 0, 5, key)<<endl;
-    // if (ans)
-    // {
-    //     cout << "Element is Present" << endl;
-    // }
-    // else
-    // {
-    //     cout << "Element is Absent" << endl;
-    // }
-    return 0;
+ 
+int main() {
+ 
+ int arr[100], num, i, n, beg, end;
+ 
+ cout <<"Enter the size of an array (Max 100) \n";
+ cin >> n;
+ 
+ cout <<"Enter the sorted values \n";
+ 
+ for(i=0; i<n; i++) {
+  
+  cin >> arr[i];
+ }
+ 
+ cout <<"Enter a value to be search \n";
+ cin >> num;
+ 
+ beg = 0;
+ end = n-1;
+ 
+ BinarySearch (arr, num, beg, end);
+ 
+ return 0;
 }
