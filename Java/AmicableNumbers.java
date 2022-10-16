ca821a62b534dd70c2e50bb5839ba1e3584ca383
Gaurav07076 <103797867+Gaurav07076@users.noreply.github.com>
2022-10-16 15:20:00 +0530

    Add files via upload

---
+++ Java/AmicableNumbers.java
@@ -0,0 +1,34 @@
+package com.topics.mathsForDSA.otherThanBitwise;
+
+public class AmicableNumbers {
+    public static void main(String[] args) {
+
+        System.out.println(isAmicable(220,284));
+
+    }
+
+    static  boolean isAmicable(int a
+
+             , int b){
+        if (a == factors2(b) && b == factors2(a)){
+            return true;
+        }
+        return false;
+    }
+
+    static int factors2(int n) {
+        int sum = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == n) {
+                    sum = sum + 1;
+                }else if (n / i == i) {
+                    sum = sum + i;
+                }else {
+                    sum = sum + i + n/ i;
+                }
+            }
+        }
+        return sum;
+    }
+}
