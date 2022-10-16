3d1b34ccf61829895a4b9ba96e39a3f26facc689
gauravsahu494 <73162277+gauravsahu494@users.noreply.github.com>
2022-10-16 11:39:35 +0530

    Update : Added Selection Sort in C++
    Added Selection_sort.cpp

---
+++ Selection_sort.cpp
@@ -0,0 +1,47 @@
+// C++ program for implementation of selection sort
+#include <bits/stdc++.h>
+using namespace std;
+
+//Swap function
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+	for (i = 0; i < n-1; i++)
+	{
+		min_idx = i;
+		for (j = i+1; j < n; j++)
+		if (arr[j] < arr[min_idx])
+			min_idx = j;
+		if(min_idx!=i)
+			swap(&arr[min_idx], &arr[i]);
+	}
+}
+
+//Function to print an array
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// Driver program to test above functions
+int main()
+{
+	int arr[] = {13, 1, 6, 22, 11};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	selectionSort(arr, n);
+	cout << "Sorted array: \n";
+	printArray(arr, n);
+	return 0;
+}
+
+// This code is contributed by Gaurav Sahu
