27b2fa872e22c19a0a61612bb1876272563650aa
Anushka Singh <98011558+Anushka-Singh1@users.noreply.github.com>
2022-10-18 13:58:49 +0530

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n, num, digit, rev=0;
+ cout<<"Enter a positive number:";
+ cin>>num;
+ n=num;
+ do
+ {
+     digit=num%10;
+     rev=(rev*10)+digit;
+     num=num/10;
+
+ }
+ while(num!=0);
+ cout<<"The reverse of the number is:"<<rev<<endl;
+ if(n==rev)
+    cout<<"the number is palindrome.";
+ else
+    cout<<"the number is not a palindrome.";
+ return 0;
+}
+
+
