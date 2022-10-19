25a91da74d0c340fec784be28f279e36509c2298
Mahi-222 <116137771+Mahi-222@users.noreply.github.com>
2022-10-19 09:48:14 +0530

    Create get_subscription.cpp

---
+++ CPP/get_subscription.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x<=30)
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
