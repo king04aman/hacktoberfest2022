6ca9468a9fcda0da9602047156ea169392cd1ed6
unknown <vimarshjain321@gmail.com>
2022-10-19 23:38:10 +0530

    Contributed in Prime number checking problem

--- C/primeNumber.c
+++ C/primeNumber.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
-
-int main(){
-int n;
-printf("enter a number: ");
-scanf("%d",&n);
-int count =0;
-for(int i=1; i<=n; i++){
-if(n%i == 0){
-    count++;
-}
-}
-if(count == 2){
-    printf("the number %d is prime",n);
-}else{
-    printf("the number is not prime");
-}
-
-    return 0;
-}
+#include<stdio.h>  
+int main(){    
+int n,i,m=0,flag=0;    
+printf("Enter the number to check prime:");    
+scanf("%d",&n);    
+m=n/2;    
+for(i=2;i<=m;i++)    
+{    
+if(n%i==0)    
+{    
+printf("Number is not prime");    
+flag=1;    
+break;    
+}    
+}    
+if(flag==0)    
+printf("Number is prime");     
+return 0;  
+ }    
