e69baf4a821a5022429582fe6a84fd73e56c38e5
ShreyaDubey1001 <115868572+ShreyaDubey1001@users.noreply.github.com>
2022-10-19 22:58:20 +0530

    Update Fibonacci series.c
    If user gives negative value as number of elements then the code must be able to handle this situation. If code continues it will produce wrong series. Hence this condition  must be included.

--- C/Fibonacci series.c
+++ C/Fibonacci series.c
@@ -3,7 +3,11 @@ int main()
 {    
  int n1=0,n2=1,n3,i,number;    
  printf("Enter the number of elements:");    
- scanf("%d",&number);    
+ scanf("%d",&number); 
+ if(number<0){
+  printf("Number of elements cannot be negative\n");
+  return 0;
+ }
  printf("\n%d %d",n1,n2);//printing 0 and 1    
  for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
  {    
