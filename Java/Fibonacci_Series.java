6dc076bba3252d6d19736d2380c6b1cebd740aec
ShreyaDubey1001 <115868572+ShreyaDubey1001@users.noreply.github.com>
2022-10-19 23:20:34 +0530

    Update Fibonacci_Series.java
    Now code is able to print fibonacci series upto desired number of terms. Also it will handle the condition if entered number of elements of the series are positive or not.
    For negative input the code wont be executed further with a error message. Thus code is modified into user friendly...

--- Java/Fibonacci_Series.java
+++ Java/Fibonacci_Series.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class FibonacciCalc{
 	public static int fibonacciRecursion(int n){
 	if(n == 0){
@@ -9,9 +10,15 @@ public class FibonacciCalc{
 	return fibonacciRecursion(n-2) + fibonacciRecursion(n-1);
 	}
     public static void main(String args[]) {
-	int maxNumber = 10;
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter number of elements:");
+	 int maxNumber = sc.nextInt();
+	   if(maxNuber<0){
+		   System.out.println("Number of elements cannot be negative");
+		   return;
+	   }
 	System.out.print("Fibonacci Series of "+maxNumber+" numbers: ");
-	for(int i = 0; i < maxNumber; i++){
+	for(int i = 0; i < =maxNumber; i++){
 			System.out.print(fibonacciRecursion(i) +" ");
 		}
 	}
