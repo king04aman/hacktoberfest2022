d4aaacc77d3e84a8d6bc8d6249c1ea247dc90ef0
Muhammad Taham <taha_shahid@outlook.com>
2022-10-21 20:09:53 +0500

    add(Javascript Code):Bubble sort algoritham

---
+++ Javascript/Fibonacci Series/Fibonacci.js
@@ -0,0 +1,14 @@
+
+// Q: Write a program to print the Fibonacci series up to a given number.
+
+const number = parseInt(prompt('Enter Any Number: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
