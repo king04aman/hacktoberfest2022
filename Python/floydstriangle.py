0ea811bab8c8b28d860a5760942225b7853adb50
amakarivas <59210780+amakarivas@users.noreply.github.com>
2022-10-21 16:25:28 +0100

    Update floydstriangle.py

--- Python/floydstriangle.py
+++ Python/floydstriangle.py
@@ -1,11 +1,33 @@
-# Floyds Triangle in Python 
-#Reading number of row
-row= int(input('Enter number of rows:'))
-#setting number to 1
-number = 1
-print("Floyd's triangle is:")
-for i in range (1,row +1):
-    for j in range (1, i+1):
-        print(number,end = "\t")
-        number += 1
+# Floyds Triangle in Python
+#Floyd’s triangle is an arrangement of natural numbers from left to right in a right triangle.
+#The first line contains a 1, the second line contains 2 and 3.
+#The next line contains 4, 5 and 6. The numbers continue infinitely in this pattern.
+n = int(input("Enter number of rows of floyd`s trangle: "))
+k = 1
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(k, end=" ")
+        k = k + 1
     print()
+    
+        
+       
+
+
+
+
+
+
+    
+        
+        
+ 
+
+  
+
+
+
+    
+        
+        
+    
