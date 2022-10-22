4408d0d7df6fb95cf01f6ef7f4d31c902446f1ee
Shashank K V <61016233+Shashank-K-V@users.noreply.github.com>
2022-10-22 14:41:17 +0530

    Create Diff.py

---
+++ Python/Diff.py
@@ -0,0 +1,7 @@
+# Asking Value of first number
+num1=int(input("Enter The Value 1st Number: "))
+# Asking Value of second number
+num2=int(input("Enter The Value 2nd Number: "))
+diffe=num1-num2
+
+print("The Difference of two numbers",diffe)
