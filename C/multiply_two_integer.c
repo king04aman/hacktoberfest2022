09ee957023c345ee347c66d617ee588076cd9765
Dhreetisahu <91011993+Dhreetisahu@users.noreply.github.com>
2022-10-22 10:04:10 +0530

    Create multiply_two_integer.c

---
+++ C/multiply_two_integer.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+	int num1,num2,product;
+	printf("Enter two numbers:");
+	scanf("%d %d",&num1,&num2);
+	product=num1*num2;
+	printf("Product of two numbers: %d",product);
+	return  0;
+}
