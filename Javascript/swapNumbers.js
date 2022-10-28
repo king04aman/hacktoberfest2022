b8b05a18e90e3619a6127671cc03d6bf5c3c35c2
Priya Singh <92670560+PriyaSingh03@users.noreply.github.com>
2022-10-28 11:59:33 +0530

    Created swapNumbers.js

---
+++ Javascript/swapNumbers.js
@@ -0,0 +1,10 @@
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
